e5c61039a3dd9ee454f53374fd71160a271b828d
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-01-16 09:57:14 +0700

    Init project base

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 
+import java.util.Scanner;
+
 /**
  *
  * @author hoang
@@ -13,7 +15,21 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("CALCULATE POSTFIX EQUATION");
+            System.out.print("Enter equation: ");
+            String equation = sc.nextLine();
+
+            // Call calculate function here
+
+            System.out.print("Do you want to continue (Y/N): ");
+            String con = sc.nextLine();
+            if (con.charAt(0) == 'N' || con.charAt(0) == 'n') {
+                break;
+            }
+        } while (true);
     }
     
 }
